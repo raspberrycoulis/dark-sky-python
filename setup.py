6805490ab7b69ca43cb8c4e42b504688faf20a6d
Wesley Archer <wesleyarcher@hotmail.com>
2018-06-27 13:06:43 +0100

    Updated setup for my repo

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name="darksky",
-      version="1.0",
+      version="2.0",
       description="A wrapper for the Dark Sky (formerly forecast.io) weather API.",
-      url="https://github.com/ratorx/dark-sky-python",
-      author="Reetobrata Chatterjee",
-      author_email="reetobratachatterjee@gmail.com",
+      url="https://github.com/raspberrycoulis/dark-sky-python",
+      author="Wesley Archer",
+      author_email="hello@wesleyarcher.com",
       license="MIT",
       packages=["darksky"],
       install_requires=["requests"])
